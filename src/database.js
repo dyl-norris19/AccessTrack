9b1800a5e9b75812fa5c07685d92668cae12612c
Larimer, Josiah <josiahlarimer@my.unt.edu>
2024-03-15 11:37:51 -0500

    Added more methods to database.js
    dealing with ratings and reports

--- src/database.js
+++ src/database.js
@@ -29,13 +29,66 @@ async function retrievePins() {
   }
 }
 
+// retreives all reports from the database, and returns said reports
+async function retrieveReports() {
+  try {
+    const q = query(collection(db, "report"));
+    const doc = await getDocs(q);
+    return doc;
+    // doc.forEach((doc) => {
+    //   console.log(doc.id, " => ", doc.data());
+    // });
+  } catch (err) {
+    console.error(err);
+    alert("An error occured while fetching pins");
+  }
+}
+
+// retreives all ratings from the database, and returns said ratings
+async function retrieveRatings() {
+  try {
+    const q = query(collection(db, "pinRating"));
+    const doc = await getDocs(q);
+    return doc;
+    // doc.forEach((doc) => {
+    //   console.log(doc.id, " => ", doc.data());
+    // });
+  } catch (err) {
+    console.error(err);
+    alert("An error occured while fetching pins");
+  }
+}
+
+// creates a pin in the database
 async function createPin(pin) {
   try {
     const docRef = await addDoc(collection(db, "pins"), pin);
-    console.log("Document written with ID: ", docRef.id);
+    console.log("Pin document written with ID: ", docRef.id);
   } catch (err) {
     console.error(err);
     alert("An error occured while creating a pin");
+  }
+}
+
+// creates a report in the database
+async function createReport(report) {
+  try {
+    const docRef = await addDoc(collection(db, "report"), report);
+    console.log("Report document written with ID: ", docRef.id);
+  } catch (err) {
+    console.error(err);
+    alert("An error occured while creating a report");
+  }
+}
+
+// creates a pin rating in the database
+async function createRating(rating) {
+  try {
+    const docRef = await addDoc(collection(db, "pinRating"), rating);
+    console.log("Rating document written with ID: ", docRef.id);
+  } catch (err) {
+    console.error(err);
+    alert("An error occured while creating a rating");
   }
 }
 
@@ -52,9 +105,10 @@ function getCurrentUserId() {
   });
 }
 
+/****************************************************************************************************************************************************************************/
 // react component as an example to display all this stuff, and show how to use it
 function DatabaseStuff() {
-  // helper function to retrieve all pins from the database
+  // demonstration function to retrieve all pins from the database
   async function retrievePinsMine() {
     try {
       const doc = await retrievePins();
@@ -85,11 +139,81 @@ function DatabaseStuff() {
     });
   }
 
+  // helper function to create a report in the database
+  async function createReportMine() {
+    getCurrentUserId().then((uid) => {
+      //const userRef = db.collection("users").doc(uid);
+      const report = {
+        title: "Another Bad Pin",
+        description: "This is worse than the last one!",
+        pinID: "/pins/1234",
+        creatorID: uid,
+      };
+      try {
+        createReport(report);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+
+  // demonstration function to retrieve all reports from the database
+  async function retrieveReportsMine() {
+    try {
+      const doc = await retrieveReports();
+      doc.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // function to create a pin rating in the database
+  async function createRatingMine() {
+    getCurrentUserId().then((uid) => {
+      //const userRef = db.collection("users").doc(uid);
+      const rating = {
+        accuracy: 4,
+        creatorID: uid,
+        description: "This is a great pin!",
+        pinID: "/pins/1234",
+        quality: 5,
+        timestamp: Timestamp.now(),
+      };
+      try {
+        createRating(rating);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+
+  // demonstration function to retrieve all pins from the database
+  async function retrieveRatingsMine() {
+    try {
+      const doc = await retrieveRatings();
+      doc.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <Header headerTitle={"Database Stuff"} />
       <button onClick={retrievePinsMine}>Click me to retrieve pins</button>
       <button onClick={createPinMine}>Click me to create a pin</button>
+      <button onClick={createReportMine}>Click me to create a report</button>
+      <button onClick={retrieveReportsMine}>
+        Click me to retrieve reports
+      </button>
+      <button onClick={createRatingMine}>Click me to create a rating</button>
+      <button onClick={retrieveRatingsMine}>
+        Click me to retrieve ratings
+      </button>
     </div>
   );
 }
