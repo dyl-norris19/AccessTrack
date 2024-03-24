8e422b34b78a08b2b0c3caf96628d74bce93b204
Haley Ford <158205631+haleynf@users.noreply.github.com>
2024-03-24 13:23:46 -0500

    Update Template.js

--- src/Template.js
+++ src/Template.js
@@ -1,29 +1,101 @@
-function Header() {
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+
+function LoginButton() {
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <a href="/dashboard" className="text-light">
+        Profile
+      </a>
+    );
+  }
+  return (
+    <a href="/login" className="text-light">
+      Login
+    </a>
+  );
+}
+
+function CreatePinButton() {
+  const [user, loading, error] = useAuthState(auth);
+  
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <a href="/createPin" className="text-light">
+        Create Pin
+      </a>
+    );
+  }
+  return (
+    <a href="/login" className="text-light">
+       Create Pin
+    </a>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="header-logo">
+      <Link to="/">
+        <img src="./logo.png" alt="Logo" height="40" />
+      </Link>
+    </div>
+  );
+}
+
+export function Header({ headerTitle }) {
   return (
     <div className="header d-flex justify-content-between align-items-center">
-      <div className="header-logo">
-        <img src="../public/logo.png" alt="Logo" height="40" />
+      {/* <div className="header-logo">
+        <img src="./logo.png" alt="Logo" height="40" />
+      </div> */}
+      <Logo />
+      <h1>{headerTitle}</h1>
+      <div className="header-links" align="left"> {/*problem*/}
+        <a href="/map" className="text-light">
+          Map
+        </a>
       </div>
-      <h1>Header Title</h1>
       <div className="header-links">
         <a href="/" className="text-light">
           Home
         </a>
-        <a href="#" className="text-light">
+        <a href="/about" className="text-light">
           About Us
         </a>
-        <a href="#" className="text-light">
-          (either login and sign up, or logout)
-        </a>
+        <LoginButton />
+        <CreatePinButton />
       </div>
     </div>
   );
 }
 
-function Footer() {
+export function Footer() {
+  const centeredTextStyle = {
+    flex: 1,
+    textAlign: "center",
+    margin: 0,
+  };
   return (
-    <div className="footer">
-      <p>Team Sad Cat FTW!</p>
+    <div className="footer d-flex justify-content-between align-items-center">
+      <p style={centeredTextStyle}>Team Sad Cat FTW!</p>
+      <div>
+        <a href="/register" className="text-light me-3">
+          Create an Account
+        </a>
+        <a href="/logout" className="text-light">
+          Logout
+        </a>
+      </div>
     </div>
   );
 }
@@ -34,10 +106,10 @@ function Template({ headline }) {
   return (
     <div
       className="d-flex flex-column min-vh-100"
-      style={{ "background-color": "#444040" }}
+      style={{ backgroundColor: "#444040" }}
     >
-      <Header />
-      <div class="content flex-grow-1">
+      <Header headerTitle={"Template Page"} />
+      <div className="content flex-grow-1">
         <h2>{headline}</h2>
       </div>
       <Footer />
