8ffd2ede4ec15d10c52e4ef625dfc0548efdd936
Haley Ford <158205631+haleynf@users.noreply.github.com>
2024-03-24 20:20:29 -0500

    added map route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Template from "./Template";
+import AdminDashboard from "./admin-portal/AdminDashboard";
+import { Login, Register, Reset, Logout, EditProfile } from "./authentication";
+import AboutUs from "./AboutUs";
+import { DatabaseStuff } from "./database";
+import CreatePin from "./CreatePin";
+import Map from "./Map";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Template headline="index.js -> App.js -> Template.js" />}
-        />
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="/logout" element={<Logout />} />
+        <Route exact path="/about" element={<AboutUs />} />
+        <Route exact path="/database" element={<DatabaseStuff />} />
+        <Route exact path="/dashboard" element={<EditProfile />} />
+        <Route exact path="/createPin" element={<CreatePin />} />
+        <Route exact path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
