40b74a6a4cc6d81c4d6c40d1cbae2dfff7d30341
Haley Ford <158205631+haleynf@users.noreply.github.com>
2024-03-24 20:23:46 -0500

    replace google map with mapbox map

--- src/Map.js
+++ src/Map.js
@@ -8,14 +8,12 @@ function Map() {
     >
       <Header headerTitle={"Map"} />
         <div
-          style={{width: "800", height: "600", style: "border:0;", allowfullscreen: "", loading: "lazy", referrerpolicy: "no-referrer-when-downgrade"}}
-        >
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d53412.27481435245!2d-97.1276288!3d33.2070912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1711310881678!5m2!1sen!2sus"></iframe>
+          style={{title: "Navigation", style: "border:none;"}}
+       >
+        <iframe width='100%' height='613px' src="https://api.mapbox.com/styles/v1/haleynf/cltvx0hq2008j01nogmol3zqa.html?title=copy&access_token=pk.eyJ1IjoiaGFsZXluZiIsImEiOiJjbHN5c3hzeTcwZ2pwMmltdXUzdHprYWlsIn0._n2hM1vDIHvaBV8fTORxIw&zoomwheel=true&fresh=true#13.09/33.2084/-97.14749"></iframe>          
         </div>
       
     </div>
-    
-    
     
   );
 }
