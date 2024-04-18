3f1b7641b43f9ef463f7cadf69364bd981e20723
Larimer, Josiah <josiahlarimer@my.unt.edu>
2024-04-18 11:49:47 -0500

    removed create pin button from header

--- src/Template.js
+++ src/Template.js
@@ -64,7 +64,7 @@ export function Header({ headerTitle }) {
           About Us
         </a>
         <LoginButton />
-        <CreatePinButton />
+        {/* <CreatePinButton /> */}
       </div>
     </div>
   );
